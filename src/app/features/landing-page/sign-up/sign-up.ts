7f40cbf9c4478948cc4f15f06e1ec875bcb7b57b
Daniel <Danielluzius@mail.de>
2025-11-05 10:36:51 +0100

    feat: sign-up page without final styling

--- src/app/features/landing-page/sign-up/sign-up.ts
+++ src/app/features/landing-page/sign-up/sign-up.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './sign-up.html',
-  styleUrl: './sign-up.scss'
+  styleUrl: './sign-up.scss',
+  standalone: true,
 })
 export class SignUp {
+  name = '';
+  email = '';
+  password = '';
+  confirmPassword = '';
+  acceptPrivacyPolicy = false;
 
+  constructor(private router: Router) {}
+
+  onSignUp() {
+    // Sign-up-Logik kommt später
+    console.log('Sign up clicked');
+  }
+
+  goBackToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
