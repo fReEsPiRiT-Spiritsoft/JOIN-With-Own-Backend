04cc960059f82a449824aa67a5ef06d00f38cfd1
fReEsPiRiT-Spiritsoft <p.schmidt.391994@gmail.com>
2025-11-06 13:23:19 +0100

    Feature: Checks if the user is logged in; if not, the navbar only shows a link to the login page.

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../../core/services/auth-service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
+  standalone: true
 })
-export class Navbar {}
+export class Navbar implements OnInit {
+  private authService = inject(AuthService);
+  
+  isLoggedIn = false;
+
+  ngOnInit() {
+    this.checkLoginStatus();
+    this.authService.currentUser$.subscribe(user => {
+      this.isLoggedIn = user !== null;
+    });
+  }
+
+  private checkLoginStatus() {
+    this.isLoggedIn = this.authService.isLoggedIn();
+  }
+}
