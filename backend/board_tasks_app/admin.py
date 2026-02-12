24cbbe67e300a2731f728b9102cc95c10384bf47
fReEsPiRiT-Spiritsoft <p.schmidt.391994@gmail.com>
2026-02-12 09:56:38 +0100

    add admin.py

--- backend/board_tasks_app/admin.py
+++ backend/board_tasks_app/admin.py
@@ -6,7 +6,14 @@ class TaskAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'isPrivate', 'ownerId')
     search_fields = ('title', 'ownerId')
     list_filter = ('isPrivate',)
+    class Meta:
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
 
+    def __str__(self):
+        return self.title
+    
+    
 @admin.register(BoardSettings)
 class BoardSettingsAdmin(admin.ModelAdmin):
     list_display = ('id', 'userId')
--- backend/contacts_app/admin.py
+++ backend/contacts_app/admin.py
@@ -5,3 +5,10 @@ from .models import Contact
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('id', 'email', 'phone')
     search_fields = ('email',)
+
+    class Meta:
+        verbose_name = "Kontakt"
+        verbose_name_plural = "Kontakte"
+
+    def __str__(self):
+        return self.email
